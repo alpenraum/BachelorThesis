9cf32354526f7f89f94928b93360aff5a9754173
Finn Zimmer <finn@zimmers.at>
2021-07-19 19:14:35 +0200

    added settings tab

--- app/src/main/java/com/example/bachelorthesis/MainActivity.java
+++ app/src/main/java/com/example/bachelorthesis/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.bachelorthesis;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
     private final static String INIT_LOC = "firstStart";
 
+    private Fragment settingsFragment = null;
+
+    private boolean settingsVisible = false;
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //Lock orienttation based on device
+        //Lock orientation based on device
         if(getResources().getBoolean(R.bool.portrait_only)){
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
         }else{
@@ -52,4 +64,62 @@ public class MainActivity extends AppCompatActivity {
     private void loadData(){
 
     }
+    private void showSettings(){
+        View gray =  findViewById(R.id.fadeBackground);
+        gray.setVisibility(View.VISIBLE);
+        gray.animate().alpha(0.4f);
+
+        settingsFragment= SettingsFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+
+        fragmentTransaction.setCustomAnimations(R.anim.slide_in_right,R.anim.slide_out_right);
+        fragmentTransaction.setReorderingAllowed(true).add(R.id.main_settings_fragment_container,
+                settingsFragment).addToBackStack(null).commit();
+
+        settingsVisible = true;
+    }
+    private void hideSettings(){
+
+        getSupportFragmentManager().beginTransaction().remove(settingsFragment).commit();
+
+        settingsFragment = null;
+
+        View gray =  findViewById(R.id.fadeBackground);
+        gray.animate().alpha(0.0f).withEndAction(() -> {gray.setVisibility(View.GONE);});
+
+        settingsVisible = false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                if(!settingsVisible) {
+                    item.setIcon(R.drawable.settings_to_cross);
+                    AnimatedVectorDrawable avd = (AnimatedVectorDrawable) item.getIcon();
+                    avd.start();
+                    showSettings();
+                }else{
+                    item.setIcon(R.drawable.cross_to_settings);
+                    AnimatedVectorDrawable avd = (AnimatedVectorDrawable) item.getIcon();
+                    avd.start();
+                    hideSettings();
+                }
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/bachelorthesis/SettingsFragment.java
@@ -0,0 +1,56 @@
+package com.example.bachelorthesis;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SettingsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SettingsFragment newInstance() {
+        SettingsFragment fragment = new SettingsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button deleteButton = v.findViewById(R.id.settings_delete_button);
+
+
+        deleteButton.setOnClickListener(v1 -> {
+        }); //todo: add deleting function
+
+        return v;
+    }
+}
